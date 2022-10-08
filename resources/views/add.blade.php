9c4f1f9b156ef196aa4a40f55adbeb6abdcad269
sahala123 <sahala21094@gmail.com>
2022-10-08 12:07:37 +0100

    error message

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -39,7 +39,7 @@
                         @if (count($errors) > 0)
                                     <!-- Form Error List -->
                                     <div class="alert alert-danger">
-                                        <strong>Whoops! Something went wrong!</strong>
+                                        <strong>Oops! Something went wrong!</strong>
                                         <br><br>
                                         <ul>
                                             @foreach ($errors->all() as $error)
